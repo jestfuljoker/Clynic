0121784dd5b03e562e1a609bded6b537af169ffb
Christofer Assis <chris.f.assis18@gmail.com>
2021-03-26 15:06:16 -0300

    Refatorado codigo da rota de agendamentos

---
+++ src/models/Appointment.ts
@@ -0,0 +1,17 @@
+import { v4 as uuid } from 'uuid';
+
+class Appointment {
+  id: string;
+
+  provider: string;
+
+  date: Date;
+
+  constructor(provider: string, date: Date) {
+    this.id = uuid();
+    this.provider = provider;
+    this.date = date;
+  }
+}
+
+export default Appointment;
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,11 +1,28 @@
 import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
-
-const appointments = [];
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.post('/', (request, response) => {
-  return response.json({ message: 'hello world' });
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: ' This appointment is alredy booked.' });
+  }
+
+  const appointment = appointmentsRepository.create(provider, parsedDate);
+
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
