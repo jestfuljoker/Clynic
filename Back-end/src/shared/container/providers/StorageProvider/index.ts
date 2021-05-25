a8fc45421915f7968fa460b3265998decddc5aa6
Chriszao <chris.f.assis18@gmail.com>
2021-05-25 19:42:47 -0300

    Configurado amazon ses e S3

---
+++ Back-end/src/shared/container/providers/StorageProvider/index.ts
@@ -0,0 +1,13 @@
+import { container } from 'tsyringe';
+
+import IStorageProvider from './models/IStorageProvider';
+
+import DiskStorageProvider from './implementations/DiskStorageProvider';
+import S3StorageProvider from './implementations/S3StorageProvider';
+
+const providers = {
+  disk: DiskStorageProvider,
+  s3: S3StorageProvider,
+};
+
+container.registerSingleton<IStorageProvider>('StorageProvider', providers.s3);
--- Back-end/src/shared/container/providers/index.ts
+++ Back-end/src/shared/container/providers/index.ts
@@ -1,18 +1,15 @@
 import { container } from 'tsyringe';
 
-import IStorageProvider from './StorageProvider/models/IStorageProvider';
-import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
+import mailConfig from '@config/mail';
+
+import './StorageProvider';
 
 import IMailProvider from './MailProvider/models/IMailProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
+import SESMailProvider from './MailProvider/implementations/SESMailProvider';
 
 import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
 import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider,
-);
 
 container.registerSingleton<IMailTemplateProvider>(
   'MailTemplateProvider',
@@ -21,5 +18,7 @@ container.registerSingleton<IMailTemplateProvider>(
 
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  container.resolve(EtherealMailProvider),
+  mailConfig.driver === 'ethereal'
+    ? container.resolve(EtherealMailProvider)
+    : container.resolve(SESMailProvider),
 );
