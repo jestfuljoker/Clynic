e97570040d91af4a61626263f5330c549c598285
mahcassi <mariaed.cassiano@gmail.com>
2021-05-25 16:35:55 -0300

    Validação Login

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-// import SignIn from './pages/SignIn';
+import SignIn from './pages/SignIn';
 
-import SignUp from './pages/SignUp';
+// import SignUp from './pages/SignUp';
 
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
     <>
-        <SignUp />
+        <SignIn />
         <GlobalStyle />
     </>
 );
