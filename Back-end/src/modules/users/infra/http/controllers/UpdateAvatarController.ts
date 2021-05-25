62ae648c01fe3057ae27a6177c5e40d007ce6611
Chriszao <chris.f.assis18@gmail.com>
2021-05-25 15:34:05 -0300

    Aplicado class transformer

--- Back-end/src/modules/users/infra/http/controllers/UpdateAvatarController.ts
+++ Back-end/src/modules/users/infra/http/controllers/UpdateAvatarController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+import { classToClass } from 'class-transformer';
 
 export default class UpdateAvatarController {
   public async update(request: Request, response: Response): Promise<Response> {
@@ -12,15 +13,6 @@ export default class UpdateAvatarController {
       avatarFilename: request.file.filename,
     });
 
-    const userWithoutPassword = {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      avatar: user.avatar,
-      created_at: user.created_at,
-      updated_at: user.updated_at,
-    };
-
-    return response.json(userWithoutPassword);
+    return response.json(classToClass(user));
   }
 }
