b31d8a412868ea3db0efa6469987c9cadeec5554
mahcassi <mariaed.cassiano@gmail.com>
2021-05-26 20:22:23 -0300

    reajustes

--- front-end/src/components/ToastContainer/Toast/index.tsx
+++ front-end/src/components/ToastContainer/Toast/index.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react';
 import {
-    FiAlertCircle,
-    FiCheckCircle,
-    FiInfo,
-    FiXCircle,
+  FiAlertCircle,
+  FiCheckCircle,
+  FiInfo,
+  FiXCircle,
 } from 'react-icons/fi';
 
 import { Container } from './styles';
@@ -11,41 +11,41 @@ import { Container } from './styles';
 import { ToastMessage, useToast } from '../../../hooks/toast';
 
 interface ToastProps {
-    message: ToastMessage;
+  message: ToastMessage;
 }
 const icons = {
-    info: <FiInfo size={24} />,
-    error: <FiAlertCircle size={24} />,
-    success: <FiCheckCircle size={24} />,
+  info: <FiInfo size={24} />,
+  error: <FiAlertCircle size={24} />,
+  success: <FiCheckCircle size={24} />,
 };
 
 const Toast: React.FC<ToastProps> = ({ message }) => {
-    const { removeToast } = useToast();
+  const { removeToast } = useToast();
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            removeToast(message.id);
-        }, 3000);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      removeToast(message.id);
+    }, 3000);
 
-        return () => {
-            clearTimeout(timer);
-        };
-    }, [removeToast, message.id]);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [removeToast, message.id]);
 
-    return (
-        <Container type={message.type} hasDescription={!!message.description}>
-            {icons[message.type || 'info']}
+  return (
+    <Container type={message.type} hasDescription={!!message.description}>
+      {icons[message.type || 'info']}
 
-            <div>
-                <strong>{message.title}</strong>
-                {message.description && <p>{message.description}</p>}
-            </div>
+      <div>
+        <strong>{message.title}</strong>
+        {message.description && <p>{message.description}</p>}
+      </div>
 
-            <button onClick={() => removeToast(message.id)} type="button">
-                <FiXCircle size={18} />
-            </button>
-        </Container>
-    );
+      <button onClick={() => removeToast(message.id)} type="button">
+        <FiXCircle size={18} />
+      </button>
+    </Container>
+  );
 };
 
 export default Toast;
