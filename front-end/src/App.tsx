85e71a46ccccb4ad5f810784384425dc11a182be
mahcassi <mariaed.cassiano@gmail.com>
2021-05-25 21:41:39 -0300

    Login pelo Contexto

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -4,17 +4,18 @@ import SignIn from './pages/SignIn';
 
 // import SignUp from './pages/SignUp';
 
-import AuthContext from './context/AuthContext';
+import { AuthProvider } from './context/AuthContext';
 
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
     <>
-        <AuthContext.Provider value={{ name: 'Maria' }}>
+        <AuthProvider>
             <SignIn />
-        </AuthContext.Provider>
+        </AuthProvider>
 
         <GlobalStyle />
     </>
 );
+
 export default App;
