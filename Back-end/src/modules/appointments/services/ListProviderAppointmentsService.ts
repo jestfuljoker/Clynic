7a7b35b03cf7c6cb08347a6fd31b211ea20022a8
Chriszao <chris.f.assis18@gmail.com>
2021-05-26 17:21:34 -0300

    Serializacao de cache

--- Back-end/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ Back-end/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,4 +1,5 @@
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 import { injectable, inject } from 'tsyringe';
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
@@ -42,7 +43,7 @@ class ListProviderAppointmentsService {
         },
       );
 
-      await this.cacheProvider.save(cacheKey, appointments);
+      await this.cacheProvider.save(cacheKey, classToClass(appointments));
     }
 
     return appointments;
