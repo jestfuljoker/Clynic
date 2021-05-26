d10840a0489dab0445d803b53fac16dc3a919554
Chriszao <chris.f.assis18@gmail.com>
2021-05-26 17:08:10 -0300

    Reaftorado dias disponiveis e agendamentos no mesmo horario

--- Back-end/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ Back-end/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -11,9 +11,14 @@ export default class FakeAppointmentsRepository
   implements IAppointmentsRepository {
   private appointments: Appointment[] = [];
 
-  public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = this.appointments.find(appointment =>
-      isEqual(appointment.date, date),
+  public async findByDate(
+    date: Date,
+    provider_id: string,
+  ): Promise<Appointment | undefined> {
+    const findAppointment = this.appointments.find(
+      appointment =>
+        isEqual(appointment.date, date) &&
+        appointment.provider_id === provider_id,
     );
 
     return findAppointment;
