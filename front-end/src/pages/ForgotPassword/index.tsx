e887bea28004621c4fb5500dff94b5e0f982aa45
Chriszao <chris.f.assis18@gmail.com>
2021-05-27 14:32:15 -0300

    Corrigido bugs

--- front-end/src/pages/ForgotPassword/index.tsx
+++ front-end/src/pages/ForgotPassword/index.tsx
@@ -95,7 +95,7 @@ const ForgotPassword: React.FC = () => {
             </Button>
           </Form>
 
-          <Link to="./signin">
+          <Link to="/">
             <FiLogIn />
             Voltar ao Login
           </Link>
