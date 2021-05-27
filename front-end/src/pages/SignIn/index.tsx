b468fe92aa3071eaba5b6340b4f2bc4f54ffe646
mahcassi <mariaed.cassiano@gmail.com>
2021-05-27 13:11:54 -0300

    Ajustes page Forgot Password

--- front-end/src/pages/SignIn/index.tsx
+++ front-end/src/pages/SignIn/index.tsx
@@ -93,7 +93,7 @@ const SignIn: React.FC = () => {
             />
             <Button type="submit">Entrar</Button>
 
-            <a href="forgot">Esqueci minha senha</a>
+            <Link to="forgot-password">Esqueci minha senha</Link>
           </Form>
 
           <Link to="./signup">
